653ee192c555f8ac57f50c746ff48c21d92e331f
Shunpei Kobayashi <skonb@me.com>
2017-02-10 11:55:06 +0900

    Rails: token authentication

--- server/rails/c2c/app/controllers/application_controller.rb
+++ server/rails/c2c/app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
+  acts_as_token_authentication_handler_for User
+  acts_as_token_authentication_handler_for Admin
+
   protect_from_forgery with: :exception
 end
